b04d4e07f4cc27a797afc6a80a93c48d6677b5cf
ale-strada <79310707+ale-strada@users.noreply.github.com>
2024-01-17 11:52:52 -0300

    added env vars to firebase service account

--- server/db.ts
+++ server/db.ts
@@ -1,10 +1,10 @@
 import * as admin from "firebase-admin";
-// import { firestore } from "firebase-admin";
-import * as serviceAccount from "./key.json";
+require("dotenv").config();
+import { serviceAccount } from "./key";
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as any),
-  databaseURL: "https://dwf-m6-ale-default-rtdb.firebaseio.com",
+	credential: admin.credential.cert(serviceAccount as any),
+	databaseURL: "https://dwf-m6-ale-default-rtdb.firebaseio.com",
 });
 
 const baseDeDatos = admin.firestore();
---
+++ server/key.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.serviceAccount = void 0;
+exports.serviceAccount = {
+    type: "service_account",
+    project_id: "dwf-m6-ale",
+    private_key_id: process.env.PRIVATE_KEY_ID,
+    private_key: process.env.PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    client_email: "firebase-adminsdk-nqu8p@dwf-m6-ale.iam.gserviceaccount.com",
+    client_id: "118062581953030340090",
+    auth_uri: "https://accounts.google.com/o/oauth2/auth",
+    token_uri: "https://oauth2.googleapis.com/token",
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+    client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-nqu8p%40dwf-m6-ale.iam.gserviceaccount.com",
+};
